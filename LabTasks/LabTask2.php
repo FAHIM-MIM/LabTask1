4d918c75ef4890b14e7a770dad360c6177d0617f
Fahim Faisal <84770464+FAHIM-MIM@users.noreply.github.com>
2021-06-08 22:32:10 +0600

    Add files via upload

---
+++ LabTasks/LabTask2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>LabTask-2</title>
+</head>
+<body>
+	<?php
+	$nameERR = $emailERR = $dateOfBirthERR = $genderERR = $degreeERR = $bloodGroupERR = "";
+	$name = $email = $dateOfBirth = $gender = $degree = $bloodGroup = "";
+
+	if ($_SERVER['REQUEST_METHODE'] == "post")
+	<?php
+
+</body>
+</html>
